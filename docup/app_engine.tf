8d6b0f79ca596ce910e4411f0bc2956bab293d9d
napoli (HiroakiTanaka) <hi.k.tanaka@mercari.com>
2020-02-24 00:16:09 +0900

    wip

--- docup/app_engine.tf
+++ docup/app_engine.tf
@@ -1,4 +1,9 @@
 resource "google_app_engine_application" "app" {
   project     = var.project_id
   location_id = "asia-northeast1"
+
+  iap {
+    oauth2_client_id     = "965242496332-f1epc5enp7ji73e6hrq0dphc5sshlmqj.apps.googleusercontent.com"
+    oauth2_client_secret = ""
+  }
 }
--- docup/iap.tf
+++ docup/iap.tf
@@ -1,18 +1,18 @@
 resource "google_iap_web_iam_policy" "docup_iap_web_iam_policy" {
-  project = var.project_id
+  project     = var.project_id
   policy_data = "{}"
 }
 
 resource "google_iap_web_type_app_engine_iam_policy" "iap_secured_web_app_user" {
-  project = google_app_engine_application.app.project
-  app_id = google_app_engine_application.app.app_id
+  project     = google_app_engine_application.app.project
+  app_id      = google_app_engine_application.app.app_id
   policy_data = "{}"
 }
 
 resource "google_iap_app_engine_service_iam_policy" "iap_app_engine_api_web_app_user" {
-  project = google_app_engine_application.app.project
-  app_id = google_app_engine_application.app.app_id
-  service = "api"
+  project     = google_app_engine_application.app.project
+  app_id      = google_app_engine_application.app.app_id
+  service     = "api"
   policy_data = data.google_iam_policy.iap_app_engine_api_web_app_user.policy_data
 }
 
